30a28f1866392a0893c259f81a8cc42668ce5158
Anh Le <itoutsource27@generali-life.com.vn>
2024-03-29 13:56:31 +0700

    added transformer

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,14 @@
-export default () => ({});
+export default () => ({
+    'transformer': {
+        enabled: true,
+        config: {
+          responseTransforms: {
+            removeAttributesKey: true,
+            removeDataKey: true,
+          },
+          requestTransforms : {
+            wrapBodyWithDataKey: true
+          },
+        }
+      },
+});
